f650b419af85a580d482555a5a0987a9816b79e9
Wojtek Siudzinski <admin@suda.pl>
2022-02-06 21:08:07 +0100

    Fix the static url

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
 	<div
 		class="flex flex-col items-center justify-between max-w-screen-xl pb-6 mx-auto mb-6 space-y-6 border-b border-gray-300 lg:space-y-0 lg:flex-row"
 	>
-		<img class="h-7" src="static/logo.svg" alt="Call for Maintainers" />
+		<img class="h-7" src="logo.svg" alt="Call for Maintainers" />
 		<nav
 			class="flex flex-wrap items-center justify-center space-x-6 space-y-0"
 		>
