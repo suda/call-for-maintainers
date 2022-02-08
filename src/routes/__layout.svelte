fa895d6c4ca719eac78ca4e584ed694d0ae58818
Wojtek Siudzinski <admin@suda.pl>
2022-02-08 14:45:02 +0100

    Fix the empty bar under footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,7 @@
 </svelte:head>
 
 <article class="bg-white">
-	<header class="relative px-6 pt-8 pb-20 text-gray-800 bg-cover bg-blue-50 lg:px-12 lg:pb-24"  style="min-height: calc(100vh - 15rem)">
+	<header class="relative px-6 pt-8 pb-20 text-gray-800 bg-cover bg-blue-50 lg:px-12 lg:pb-22 xl:pb-22"  style="min-height: calc(100vh - 13.5rem)">
 		<div class="relative z-10">
 			<Navigation />
 
